6e67f743882de2eb4ec449bada59796ccccbb841
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2024-10-19 21:14:10 +0100

    new

---
+++ README.md
@@ -0,0 +1,6 @@
+# travis-stash
+Automated tests repository
+
+git lfs migrate import --include="*.parquet"
+
+
---
+++ input/icetrg/copy_command.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+#
+# Copy data from lxplus [22/11/2021]
+
+CERN_PATH=/afs/cern.ch/user/y/ytakahas/public/forMikael
+
+scp mamiesko@lxplus.cern.ch:$CERN_PATH/*.root .
+
+
